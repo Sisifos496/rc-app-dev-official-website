97385778ef82766a19ee38ee00c71a402c69ce33
Sisifos496 <116097380+Sisifos496@users.noreply.github.com>
2024-01-10 18:58:15 +0300

    strucure set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      
+      <div className="navbar">
+        <div className="navbar-left">
+
+        </div>
+        <div className="navbar right">
+
+        </div>
+      </div>
+      <div className="body">
+
+      </div>
+      <div className="foot">
+
+      </div>
     </div>
   );
 }
